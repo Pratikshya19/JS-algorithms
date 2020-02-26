52114a088c6ff4e3ba9dd61dea59268aa248ac31
github username <pratikshya.pp19@gmail.com>
2020-02-25 21:57:45 -0600

    Title Case

---
+++ TitleCase.js
@@ -0,0 +1,4 @@
+//Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case
+function titleCase(str) {
+    return str.toLowerCase().replace(/(^|\s)\S/g, L => L.toUpperCase());
+  }
