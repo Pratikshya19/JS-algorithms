84d96ea0e5338c68aef8e877ccbaa7dbe5d2c85d
github username <pratikshya.pp19@gmail.com>
2020-03-15 19:26:46 -0500

    sum of all positive multiples of 3 or 5 below n

--- Sum-multiples-of 3 and 5.js
+++ Sum-multiples-of 3 and 5.js
@@ -1,7 +1,8 @@
+
 function sumMults(n) {
-    var sum = 0;
-    for (var i = 1; i < n; i++) {
-      if (i % 3 == 0 || i % 5 == 0) sum += i;
-    }
+var sum = 0;
+ for (var i = 1; i < n; i++) {
+if (i % 3 == 0 || i % 5 == 0) sum += i;
+}
     return sum;
   }
