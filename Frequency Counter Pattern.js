8f3327735ebb900d46b0c985bb72eb58258638b3
github username <pratikshya.pp19@gmail.com>
2020-04-10 00:05:48 -0500

    Frequency Counter Pattern

--- Frequency Counter Pattern.js
+++ Frequency Counter Pattern.js
@@ -1,9 +1,10 @@
 //WAP called same which accepts two arrays. The function should return true if every value in the array has it's corresponding values squared in the second array. The frequency of values must be same.
-//Solution: 1. Check length
-//2. Initialize counter1 and counter2 empty object variables to store the key as element of arrays and value as its count
-//3.Loop over arrays, store key as element as value as its count in that array.
-//4.Loop over the counter1 to check if the key in arr1 = square of key in arr2
-//5. Check if 
+//Solution: 1. Check length if true go to step2
+//2. Initialize counter1 and counter2 empty object variables to store the key as element of arrays and value as its count in arr1 and arr2 respectively.
+//3.Loop over arrays, store key as element as value as its count in object variables.
+//4.Loop over the counter1 to check if its key sqaure  = key in counter2 if true go to step 5
+//5. Check if values in counter1 is equal to values in counter2. If true return true. 
+//check length, keys ie elem and values i.e count.
 function same(arr1, arr2){
 if(arr1.length !== arr2.length){
     return false;
@@ -26,3 +27,5 @@ for(let key in counter1){
 return true
 }
 console.log(same([1,2,3,4],[1,4,9,16]))
+console.log(same([1,2,3,4,2],[1,4,9,16]))
+console.log(same([1,2,3,4,2],[1,4,9,16,4]))
